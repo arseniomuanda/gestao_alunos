28759a9176efbf50ce7cb8bb71a98d6e72ebd5b0
Arsénio Muanda <manomuanda@gmail.com>
2023-04-25 03:10:55 +0100

    funcionario full

--- app/Views/componentes/header.php
+++ app/Views/componentes/header.php
@@ -37,7 +37,7 @@
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 
-      <script>
+    <script>
         //const api_be = "http://localhost:8080";
         if (!sessionStorage.logado) {
             location.href = '/login';
@@ -218,8 +218,8 @@
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <!-- <img src="/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle"> -->
-                        <span class="d-none d-md-block dropdown-toggle ps-2">{{nome}}</span>
+                        <img v-bind:src="foto" alt="Profile" class="rounded-circle">
+                        <span class="d-none d-md-block dropdown-toggle ps-2"> {{nome}}</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
